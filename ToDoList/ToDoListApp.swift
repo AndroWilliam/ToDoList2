1dbedabf4d3165b651b85fda070e83b2f0309726
Andro <androa687@gmail.com>
2025-06-24 21:19:11 +0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Andro Adel on 24/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
